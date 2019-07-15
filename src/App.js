a85b77f7f06448340b104f7bb12a91f344a6684e
fengge <fengge@.qq.com>
2019-07-15 13:02:13 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-import 
+/* 
+应用根组件
+*/
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+
+  render () {
+    return (
+      <div>App</div> 
+    )
+  }
+}
