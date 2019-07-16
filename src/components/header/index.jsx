ccd393c1932c7c3ba8bca2a612d108e899ad5369
fengge <fengge@.qq.com>
2019-07-16 20:06:04 +0800

    6

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,118 @@
 import React, {Component} from 'react'
+import {withRouter} from 'react-router-dom'
+import {Modal} from 'antd'
 
+import LinkButton from '../link-button'
+import menuList from '../../config/menuConfig'
+import {reqWeather} from '../../api'
+import storageUtils from '../../utils/storageUtils'
+import memoryUtils from '../../utils/memoryUtils'
+import {formateDate} from '../../utils/dateUtils'
 import './index.less'
-
 /*
 头部组件
  */
-export default class Header extends Component {
+class Header extends Component {
+
+  //初始状态
+  state = {
+    currentTime: formateDate(Date.now()), // 当前时间字符串
+    dayPictureUrl: '', // 天气图片url
+    weather: '', // 天气的文本
+  }
+
+  //获取当前时间---并且更新时间状态
+  getTime = ()=>{
+    this.intervalId = setInterval(()=>{
+      const currentTime = formateDate(Date.now())
+      this.setState({currentTime})
+    },1000)
+  }
+
+  //调用接口，获取当前天气--更新天气状态
+  getWeather = async ()=>{
+    //调用接口，请求异步，获取数据
+    const {dayPictureUrl,weather} = await reqWeather('北京')
+    //更新状态
+    this.setState({dayPictureUrl,weather})
+  }
+
+  //当前显示的title
+  getTitle = ()=>{
+    //得到当前的请求路径
+    const path = this.props.location.pathname
+    let title
+    menuList.forEach( item => {
+      if(item.key===path){
+        title = item.title
+      }else if(item.children){
+        //在所有的子item中查找匹配
+        const cItem = item.children.find(cItem=>path.indexOf(cItem.key)===0)
+        //如果有---匹配
+        if(cItem){
+          title = cItem.title  //拿到title
+        }
+      }
+    })
+    return title
+  }
+
+  //点击退出登录
+  logout = ()=>{
+    // 显示确认框
+    Modal.confirm({
+      content: '确定退出吗?',
+      onOk: () => {
+        console.log('OK', this)//确认this指向问题
+        // 删除保存的user数据
+        storageUtils.removeUser()
+        memoryUtils.user = {}
+
+        // 跳转到login
+        this.props.history.replace('/login')
+      }
+    })
+  }
+
+  /* 
+  第一次render()之后执行一次
+  一般在此执行异步操作: 发ajax请求/启动定时器
+  */
+ componentDidMount(){
+   //获取当前时间
+   this.getTime()
+   //获取当前天气
+   this.getWeather()
+ }
+
+ //当前组件卸载前调用该方法--清除定时器
+ componentWillUnmount(){
+   clearInterval(this.intervalId)
+ }
 
   render() {
+
+    const {dayPictureUrl,weather,currentTime} = this.state
+    const username = memoryUtils.user.username
+    //得到当前需要显示的title
+    const title = this.getTitle()
     return (
       <div className='header'>
-        Header
+        <div className="header-top">
+          <span>欢迎，{username}</span>
+          <LinkButton onClick={this.logout}>退出</LinkButton>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">{title}</div>
+          <div className="header-bottom-right">
+            <span>{currentTime}</span>
+            <img src={dayPictureUrl} alt="weather"/>
+            <span>{weather}</span>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+export default withRouter(Header)
--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,18 +1,114 @@
 import React, {Component} from 'react'
 import {Link,withRouter} from 'react-router-dom'
-import { Menu, Icon, Button } from 'antd'
+import { Menu, Icon } from 'antd'
 
 import logo from '../../assets/images/logo.png'
+//import menuList from '../../config/menuConfig'
 import './index.less'
 
 
-const { SubMenu } = Menu
+const SubMenu = Menu.SubMenu
 /*
 左侧导航组件
  */
 class LeftNav extends Component {
 
+
+  //遍历得到导航列表
+  /* 
+  根据menu的数据数组生成对应的标签数组
+  使用map() + 递归调用
+  */
+  /* menuNodes = (menuList) => {
+    return menuList.map(item => {
+      //判断是否有子组件
+      if(!item.children){
+        return (
+          <Menu.Item key={item.key}>
+            <link to={item.key}>
+              <Icon type={item.icon} />
+              <span>{item.title}</span>
+            </link>
+          </Menu.Item>
+        )
+      } else {
+        return (
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.menuNodes(item.children)}
+          </SubMenu>
+        )
+      }
+    })
+  }
+ */
+
+
+  /* 
+  根据menu的数据数组生成对应的标签数组
+  使用reduce() + 递归调用
+  */
+
+  getMenuNodes = (menuList) =>{
+    //得到当前请求路由路径
+    const path = this.props.location.pathname
+
+    return menuList.reduce((pre,item)=>{
+      //向pre添加menu.item
+      if(!item.children){
+        pre.push((
+          <Menu.Item key={item.key}>
+            <link to={item.key}>
+              <Icon type={item.icon} />
+              <span>{item.title}</span>
+            </link>
+          </Menu.Item>
+        ))
+      }else{
+        //查找一个与当前请求路径匹配的子item
+        const cItem = item.children.find(cItem=>path.indexOf(cItem.key)===0)
+        //如果存在，说明当前的子列需要打开
+        if(cItem){
+          this.openKey = item.key
+        }
+
+        // 向pre添加<SubMenu>
+        pre.push((
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        ))
+      }
+      return pre
+    },[])
+  }
+
+
+  // componentWillMount(){
+  //   this.menuNodes = this.getMenuNodes(menuList)
+  // }
+
   render() {
+
+    const path = this.props.location.pathname
+
+    //得到需要打开菜单项的key
+    const openKey = this.openKey
     return (
       <div className="left-nav">
         <Link to='/' className="left-nav-header">
@@ -21,13 +117,13 @@ class LeftNav extends Component {
         </Link>
 
         <Menu
-          defaultSelectedKeys={['/home']}
-          defaultOpenKeys={['sub1']}
+          selectedKeys={[path]}
+          defaultOpenKeys={[openKey]}
           mode="inline"
           theme="dark"
         >
           {
-            this.menuNodes
+            this.getMenuNodes
           }
         </Menu>
         {/* <Menu
