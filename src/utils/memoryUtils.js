31a24eb2a8e0b6dd4612e16b332d9838d1b2dab4
fengge <fengge@.qq.com>
2019-07-20 18:24:39 +0800

    7

--- src/utils/memoryUtils.js
+++ src/utils/memoryUtils.js
@@ -4,7 +4,8 @@
 import storageUtils from './storageUtils'
 
 //用来存储登陆用户的信息, 初始值为local中读取的user
-const user = storageUtils.getUser()
+const user = storageUtils.getUser()//???
 export default {
   user,
+  products:{}
 }
